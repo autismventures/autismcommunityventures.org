077e8a8c293ec843de59d3d3b7ade8f814dbef33
autismventures <maureen@autismcommunityventures.org>
2015-10-28 08:32:57 +0545

    Create contact-us.php

---
+++ contact-us.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Autism Community Venture || Contact-Us</title>
+	<meta name="keywords" content="autism, autism job training, autism software, autism adults, adult services autism, autism employment, autism ventures, autism entrepreneurship, code for autism autism community ventures, autism ally, autism technology, employers for autism">
+	<meta name="description" content="Autism Community Venture inspires and supports people with autism as full participants with meaningful employment.">
+
+    <!-- Bootstrap -->
+
+    <link rel="shortcut icon" type="image/png" href=""/>
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+
+    <!--[if IE]>
+        <script src="js/iscript-ieonly.js"></script>
+    <![endif]-->
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- #Bootstrap -->
+
+    <!-- LocaL CSS style sheet -->
+
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
+    <link rel="stylesheet" type="text/css" href="fonts/fonts.css">
+
+    <!-- #LocaL CSS style sheet -->
+
+    <!-- Google font library -->
+
+    <link href='https://fonts.googleapis.com/css?family=Lato:100,100italic,300,300italic,400,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- #Google font library -->
+
+</head>
+<body class="theme-index-body theme-home-body theme-home-skeleton">
+
+    <!-- Autism Community Venture Contact us page structure -->
+    
+    <div class="theme-outer-wrap theme-skeleton-starter theme-skeleton-skin" role="main">
+        <div class="theme-inner-wrap theme-skeleton-skin-inner">
+            
+            <!-- Autism Community Venture Header -->
+            
+            <header id="theme-masthead" class="theme-masthead theme-main-header theme-standalone-header">
+                <div class="theme-header-row">
+                    <div class="theme-container">
+                        
+                        <div class="theme-display-table header-gutterless">
+                            
+                            <!-- Autism Community Venture Logo -->
+                            
+                            <div class="theme-lg-3 theme-md-3 theme-sm-3 theme-xs-6 theme-table-middle-cell theme-logo-wrap">
+                                <div class="theme-col-content">
+                                    <figure>
+                                        <a href=""><img src="images/header-template/site-logo.png" alt="" /></a>
+                                    </figure>
+                                </div>
+                            </div>
+                            
+                            <!-- #Autism Community Venture Logo -->
+                            
+                            <!-- Autism Community Venture Main Navigation -->
+                            
+                            <div class="theme-lg-9 theme-md-9 theme-sm-9 col-theme-9 theme-table-middle-cell theme-nav-wrap theme-align-right">
+                                
+                            	<nav class="theme-navbar navbar navbar-default" role="navigation">
+                                  <!-- Autism Community Venture Brand and toggle get grouped for better mobile display -->
+                                    <div class="navbar-header theme-navbar-header">
+                                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#icollpase-nav" aria-expanded="false">
+                                        <span class="sr-only">Toggle navigation</span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                      </button>
+                                    </div>
+                                
+                                    <!-- Collect the nav links, forms, and other content for toggling -->
+                                    <div class="collapse navbar-collapse" id="icollpase-nav">
+                                      <ul class="nav navbar-nav theme-main-nav theme-display-table theme-inlineblock-fixer" role="menu">
+                                        <li><a href="index.html">Home</a></li>
+                                        <li><a href="about-us.html">About us</a></li>
+                                        <li><a href="contact-us.php" class="active">Contact us</a></li>
+                                      </ul>
+                                      
+                                    </div><!-- /.navbar-collapse -->
+                                </nav>
+                            	
+                            </div>
+                            
+                            <!-- #Autism Community Venture Main Navigation -->
+                            
+                        </div>
+                        
+                    </div>
+                </div>
+            </header>
+            
+            <!-- #Autism Community Venture Header -->
+            
+            <!-- Autism Community Venture Main -->
+            
+            <main id="theme-main" class="theme-main theme-section-wrap">
+                
+                <!-- Theme About us page Banner -->
+                
+                <section class="theme-innerpage-banner-section theme-contactus-banner-section theme-align-center">
+                    <div class="theme-section-row">
+                        <div class="theme-container container-fluid">
+                            <div class="theme-innerpage-banner-caption theme-banner-caption">
+                                <div class="theme-banner-caption-content">
+                                    <h1>Get In Touch!!</h1>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </section>
+                
+                <!-- #Theme About us page Banner -->
+                
+                <!-- Theme Contact us Section -->
+                
+                <section class="theme-innerpage-section theme-section theme-contacus-form-section">
+                    <div class="theme-section-row">
+                        <div class="theme-container container-fluid">
+                            <div class="theme-contactus-subrow">
+                                <div class="theme-display-table theme-no-gutter">
+                                    
+                                    <div class="theme-lg-6 theme-sm-6 theme-xs-12 theme-table-top-cell theme-contactus-form-col">
+                                        <div class="theme-contactus-form-wrap">
+                                            <h1>Contact us</h1>
+                                            <p>Please let us know if you have any questions or comments.
+                                                Email us at <a href="Info@autismcommunityventures.org">Info@autismcommunityventures.org</a> or fill out the contact form below:</p>
+                                            <form action="mail.php">
+                                                <div class="theme-form-group form-group form-first-name">
+                                                    <input type="text" name="fname" value="" placeholder="First Name" class="theme-form-control form-control" />
+                                                </div>
+                                                <div class="theme-form-group form-group form-last-name">
+                                                    <input type="text" name="lname" value="" placeholder="Last Name" class="theme-form-control form-control" />
+                                                </div>
+                                                <div class="theme-form-group form-group form-email">
+                                                    <input type="text" name="email" value="" placeholder="Email Address" class="theme-form-control form-control" />
+                                                </div>
+                                                <div class="theme-form-group form-group form-phone">
+                                                    <input type="text" name="phone" value="" placeholder="Phone" class="theme-form-control form-control" />
+                                                </div>
+                                                <div class="theme-form-group form-group form-radio-group theme-align-left">
+                                                    <input type="radio" value="I am interested in employing a person with autism" name="radio-opt" class="theme-form-control" id="potential-emp" />
+                                                    <label for="potential-emp">I am interested in employing a person with autism </label>
+
+                                                    <input type="radio" value="I am a person with autism" name="radio-opt" class="theme-form-control" id="potential-person" />                                      
+                                                    <label for="potential-person">I am a person with autism</label>
+                                                </div>
+                                                <div class="theme-form-group form-group form-message">
+                                                    <textarea placeholder="Message" name="message" class="theme-form-control form-control"></textarea>
+                                                </div>
+                                                <div class="theme-form-group form-group theme-submit-group theme-align-center">
+                                                    <input type="submit" value="Send" class="theme-submit-btn" />
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="theme-lg-6 theme-sm-6 theme-xs-12 theme-table-top-cell theme-contactus-form-col">
+                                        <div class="theme-sidebar-content theme-contactus-sidebar-content">
+                                            <div class="theme-googlemap-wrap">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3167.6489718654675!2d-122.1604634!3d37.4453965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fbb39829bbd3f%3A0xcbd49aa1e3dfe772!2s555+Bryant+St+%23141%2C+Palo+Alto%2C+CA+94301%2C+USA!5e0!3m2!1sen!2snp!4v1444062263455" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                                            </div>
+                                            <div class="theme-contactus-infor-wrap">
+                                                <ul class="sidebar-address-list">
+                                                    <li class="theme-street-address">
+                                                        <span class="theme-footer-address-label">Autism Community Ventures, 555 Bryant Street Suite 141 <br/><br/>Palo Alto, CA 94301</span>
+                                                    </li>
+                                                    <li class="theme-email-address">
+                                                        <span class="theme-footer-address-label"><a href="mailto:Info@autismcommunityventures.org">Info@autismcommunityventures.org</a></span>
+                                                    </li>
+                                                    
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                
+                <!-- #Theme Contact us Section -->
+                
+                
+            </main>
+            
+            <!-- #Autism Community Venture Main -->
+            
+            <!-- Theme Footer -->
+            
+            <footer id="theme-colphon" class="theme-colphon theme-footer theme-main-footer" role="contentinfo">
+                <div class="theme-footer-row">
+                    <div class="theme-container container-fluid">
+                        <div class="row clearfix">
+                            
+                            <div class="theme-lg-2 theme-sm-2 theme-xs-12 theme-footer-col theme-three-col" data-os-animation="zoomIn" data-os-animation-delay="0.15s">
+                                <div class="theme-footer-content">
+                                    <h4>Useful Links</h4>
+                                    <ul class="theme-footer-list">
+                                        <li><a href="index.html">Home</a></li>
+                                        <li><a href="about-us.html">About us</a></li>
+                                        <li><a href="contact-us.php">Contact us</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            
+                            <div class="theme-lg-3 theme-sm-3 theme-xs-12 theme-footer-col theme-three-col" data-os-animation="zoomIn" data-os-animation-delay="0.17s">
+                                <div class="theme-footer-content">
+                                    <h4>About us</h4>
+                                    <p>We are educators, technologists and advocates with a passion for instigating lasting social change.<br/></br>As a global community of technologists, educators and parents, we are on a mission to change that. </p>
+                                </div>
+                            </div>
+                    
+                            <div class="theme-lg-3 theme-sm-3 theme-xs-12 theme-footer-col theme-three-col" data-os-animation="zoomIn" data-os-animation-delay="0.20s">
+                                <div class="theme-footer-content">
+                                    <h4>Support us</h4>
+                                    <p>If your are interested in supporting us, please contact us at <a style="color: #fff; text-decoration:underline;" href="mailto:Info@autismcommunityventures.org">Info@autismcommunityventures.org</a><br/><br/>Alternatively, you can send a check to the following address: <br/>555 Bryant Street <br/>Suite 141 <br/>Palo Alto, CA 94301</p>
+                                   
+                                </div>
+                            </div>
+                    
+                            <div class="theme-lg-4 theme-sm-4 theme-xs-12 theme-footer-col theme-three-col" data-os-animation="zoomIn" data-os-animation-delay="0.22s">
+                                <div class="theme-footer-content">
+                                    <h4>Keep in Touch</h4>
+                                    <ul class="footer-address-list">
+                                        <li class="theme-street-address">
+                                            <img src="images/footer-template/address-logo.png" alt="" />
+                                            <span class="theme-footer-address-label">Autism Community Ventures<br/> 
+555 Bryant Street<br/>Suite 141<br/>
+Palo Alto, CA 94301  </span>
+                                        </li>
+                                        <li class="theme-email-address">
+                                            <img src="images/footer-template/email-logo.png" alt="" />
+                                            <span class="theme-footer-address-label"><a href="mailto:Info@autismcommunityventures.org">Info@autismcommunityventures.org</a></span>
+                                        </li>
+                                        
+                                    </ul>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
+            <div class="theme-copyright-footer">
+                <div class="theme-footer-row">
+                    <div class="theme-container container-fluid">
+                        <div class="theme-display-table theme-no-gutter">
+                            <div class="theme-lg-9 theme-sm-8 theme-xs-12 theme-copyright-col theme-table-middle-cell">
+                                <div class="theme-copyright-content">
+                                    <p>Copyright © 2015 Autism Community Ventures, All right reserved.</p>
+                                </div>
+                            </div>
+                            <div class="theme-lg-3 theme-sm-4 theme-xs-12 theme-copyright-col theme-table-middle-cell theme-align-right">
+                                <div class="theme-copyright-content theme-footer-socialmedia">
+                                    <ul class="theme-socialmedia-list theme-display-table">
+                                        <li><a href=""><img src="images/footer-template/facebook-logo.png" alt="" /></a></li>
+                                        <li><a href=""><img src="images/footer-template/twitter-logo.png" alt="" /></a></li>
+                                        <li><a href=""><img src="images/footer-template/googleplus-logo.png" alt="" /></a></li>
+                                        <li><a href=""><img src="images/footer-template/linkedin-logo.png" alt="" /></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <!-- #Theme Footer -->
+            
+        </div>
+    </div>
+    
+    <!-- #Autism Community Venture Home page structure --> 
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+    <script src="js/jquery-2.0.3.min.js"></script> 
+    <!-- Include jquery ui library -->
+    <!-- Include all compiled plugins (below), or include individual files as needed --> 
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Include easein.js --> 
+    <script src="js/jquery.easing.1.3.js"></script>  
+    <!-- Include waypoints.js --> 
+    <script src="js/jquery.waypoints.js"></script> 
+    <!-- ikentoo custom script --> 
+    <script src="js/iscript.js"></script>
+</body>
+</html>
